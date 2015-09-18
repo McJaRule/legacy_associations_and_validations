0525eead171211616d997339c452ae182b502286
Ru-T <rwajnberg@gmail.com>
2015-09-17 21:34:08 -0400

    Require unique email for user entires in database.

--- user.rb
+++ user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
 
 
   scope :want_to_be_instructors, -> { where(wants_to_be_instructor: true) }
